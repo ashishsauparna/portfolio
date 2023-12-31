4d64d98940fdb6b71e078160ad00ba0ec68d1542
Ashish <ashishsauparna@gmail.com>
2024-01-01 02:22:34 +0530

    easter

---
+++ src/app/easter/route.js
@@ -0,0 +1,3 @@
+export async function GET(req, res) {
+    return  new Response(` Easter egg 🥚: So here is the deal.... text me this - ["yorlarota you owe me a <drink-name>"] I'll owe you 10`);
+  }
