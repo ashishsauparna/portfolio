0819457bd540c535f0d488e7ad598e0cd426a3c1
Ashish <ashishsauparna@gmail.com>
2024-10-27 14:26:35 +0530

    bug fix

--- src/app/components/ui/heroimage.tsx
+++ src/app/components/ui/heroimage.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import Image from 'next/image';
-import FontSwapBanner from '/public/font_swap_banner.png';
 
 export default function HeroImage(){
     const fadeInAnimation = {
@@ -28,13 +27,15 @@ export default function HeroImage(){
         whileInView="animate"
         viewport={{
           once:true
-        }}
+        }}>
+        <div
         className='project_banner_1 inset-0 flex justify-center items-center'>
         <Image
-            src={FontSwapBanner}
+            src={"/font_swap_banner.png"}
             alt="Black and white portait of Ashish Sharma"
             sizes="100vw"
             className="w-2/4"/>
+        </div>
         </motion.div>
     )
 }
